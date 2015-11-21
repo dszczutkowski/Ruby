dd46f20d537fff3659b73de0df33faa0576ea650
dszczutkowski <dszczutkowski@outlook.com>
2015-11-21 17:29:41 +0100

    Sprecyzowanie testow

--- Animals/animal_test_spec.rb
+++ Animals/animal_test_spec.rb
@@ -3,37 +3,86 @@ require_relative 'animal'
 
 describe 'Testing Animal Class' do
 
-  it 'should get initialized values' do
-    # create object, pass variables
-    animal = Animal.new('Lio', 'lion', 2)
+  context 'to test get' do
+    it 'should get initialized name' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
 
-    # get the values
-    nameValue = animal.getName
-    speciesValue = animal.getSpecies
-    ageValue = animal.getAge
+      # get the value
+      nameValue = animal.getName
 
-    # check is value correct
-     puts nameValue, speciesValue, ageValue
+      # check is value correct
+      puts nameValue
+
+    end
+
+    it 'should get initialized species' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
+
+      # get the value
+      speciesValue = animal.getSpecies
+
+      # check is value correct
+      puts speciesValue
+
+    end
+
+    it 'should get initialized age' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
+
+      # get the value
+      ageValue = animal.getAge
+
+      # check is value correct
+      puts ageValue
+
+    end
 
   end
 
-  it 'should get set values' do
-    # create object, pass variables
-    animal = Animal.new('Lio', 'lion', 2)
-    animal.setName('Wally')
-    animal.setSpecies('cheetah')
-    animal.setAge(5)
+  context 'to test set' do
 
+    it 'should get set name' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
+      animal.setName('Wally')
 
-    # get the values
-    nameValue = animal.getName
-    speciesValue = animal.getSpecies
-    ageValue = animal.getAge
+      # get the value
+      nameValue = animal.getName
 
-    # check is value correct
-    puts nameValue, speciesValue, ageValue
+      # check is value correct
+      puts nameValue
+
+    end
+
+    it 'should get set species' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
+      animal.setSpecies('cheetah')
+
+      # get the value
+      speciesValue = animal.getSpecies
+
+      # check is value correct
+      puts speciesValue
+
+    end
+
+    it 'should get set age' do
+      # create object, pass variables
+      animal = Animal.new('Lio', 'lion', 2)
+      animal.setAge(5)
+
+      # get the value
+      ageValue = animal.getAge
+
+      # check is value correct
+      puts ageValue
+
+    end
 
   end
-
-
+  
 end
